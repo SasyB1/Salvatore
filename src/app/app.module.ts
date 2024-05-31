2a242025b9d3a4a32fcdceafc969509c665cef76
SasyB1 <164867789+SasyB1@users.noreply.github.com>
2024-05-31 12:15:02 +0200

    chore: Update Angular and Bootstrap dependencies
    This commit updates the Angular and Bootstrap dependencies in the project. The Angular dependencies in the `angular.json` and `package.json` files have been updated to version 17.3.0, and the Bootstrap dependency in the `package.json` file has been updated to version 5.3.3. Additionally, the Bootstrap CSS and JS files have been added to the `styles` and `scripts` arrays in the `angular.json` file, respectively.
    
    The purpose of this update is to ensure that the project is using the latest stable versions of Angular and Bootstrap, which may include bug fixes, performance improvements, and new features.
    
    Please note that this commit does not include any functional changes to the codebase, but rather focuses on updating the project's dependencies.
    
    ```

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { UsersCardComponent } from './components/users-card/users-card.component';
+import { HomeCardComponent } from './components/home-card/home-card.component';
+import { HomeComponent } from './pages/home/home.component';
+import { CompletedComponent } from './pages/completed/completed.component';
+import { UsersComponent } from './pages/users/users.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    UsersCardComponent,
+    HomeCardComponent,
+    HomeComponent,
+    CompletedComponent,
+    UsersComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/components/users-card/users-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-users-card',
+  templateUrl: './users-card.component.html',
+  styleUrl: './users-card.component.scss'
+})
+export class UsersCardComponent {
+
+}
